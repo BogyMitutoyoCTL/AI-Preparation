a9cc9241fda95d7a4e893d23f3e555003406768b
DaOff <daoff.do@gmail.com>
2020-03-30 13:18:48 +0200

    Algorithm written by David

---
+++ code/snake/Algorithms/David.py
@@ -0,0 +1,16 @@
+from Algorithms.Algorithms import Algorithm
+from GameData import GameData
+
+
+class David(Algorithm):
+    def __init__(self):
+        super().__init__()
+
+    def decide(self, info: GameData) -> str:
+        if info.nearest_wall_distance == 0:
+            return "turn left"
+        if info.food_y == info.head_y:
+            return "east"
+        if info.food_x == info.head_x:
+            return "north"
+        return "straight"
