8275849d3d6c2d178b38e4662af047003264f78c
Lukas Benner <lukasbenner02@gmail.com>
2020-04-01 12:34:36 +0200

    Situation Lukas angepasst um es weiter zu verwenden.

--- code/snake/Algorithms/SituationLukas.py
+++ code/snake/Algorithms/SituationLukas.py
@@ -1,20 +1,6 @@
-from Field import Field
 from GameData import GameData
-from Snake import Snake
-
-#   1    2    4
-#
-#   8    -    16
-#
-#   32   64   128
-
-
-#   1   2   4   8   16
-#   32  64  -   128 256
-
 
 class maske:
-
     def __init__(self, kantenlaenge, maskenbezeichnung: str):
         self.bezeichnung = maskenbezeichnung
         self.kantenlaenge = kantenlaenge
@@ -36,28 +22,14 @@ class maske:
                 if self.fields.pop(0) == "1":
                     self.vektoren.append((x,y))
                 else:
-                    self.vektoren.append(0)
+                    pass
 
-def situation_number(maske: maske, field: GameData):
-    ergebnis = 0
-    wertigkeit = 1
-    for i in range(0, maske.kantenlaenge ** 2):
-        if maske.vektoren[i] == 0:
-            pass
-        else:
-            vektor = tuple(maske.vektoren[i])
+    def situation_berechnen(self, field: GameData):
+        ergebnis = 0
+        wertigkeit = 1
+        for i in range(0, len(self.vektoren)):
+            vektor = tuple(self.vektoren[i])
             if field.can_move_to(field.head_x + vektor[0], field.head_y + vektor[1]) == False:
                 ergebnis += wertigkeit
             wertigkeit *= 2
-
-
-    return ergebnis # was ausgerechnetes
-
-if __name__ == "__main__":
-    field = Field(10, 20)
-    snake = Snake(field)
-    Game = GameData()
-    maske = maske(3,"111 111 111")
-    maske.vektorenBerechnen()
-    print(situation_number(maske, snake.get_info()))
-
+        return ergebnis
